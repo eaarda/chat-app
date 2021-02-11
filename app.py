60d0d323aeeaffd1411a1bf49efacbeeae15ea2c
eaarda <elifaarda@gmail.com>
2021-02-11 22:00:37 +0300

    users in a room

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, join_room
 import logging
 
 app = Flask(__name__)
@@ -22,9 +22,21 @@ def chat():
         return redirect(url_for('index'))
 
 
+@socketio.on('send_message')
+def handle_send_message_event(data):
+    app.logger.info("{} sent message to the room {}: {}".format(
+        data['username'], data['room'], data['message']))
+    socketio.emit('receive_message', data, room=data['room'])
+
+
 @socketio.on('join_room')
 def handle_join_room_event(data):
-    app.logger.info("{} joined the room {}".format(data['username'], data['room']))
+    app.logger.info("{} joined the room {}".format(
+        data['username'], data['room']))
+    join_room(data['room'])
+    """ This function puts the user in a room, under the current namespace.
+    This is a function that can only be called from a SocketIO event handler. """
+    socketio.emit('join_room_announcement', data)
 
 
 if __name__ == '__main__':
