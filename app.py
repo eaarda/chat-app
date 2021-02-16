91f02bab4ca6de16aae16729e3f7b62deaa110e1
eaarda <elifaarda@gmail.com>
2021-02-16 23:27:14 +0300

    signup implemantation done

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO, join_room
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 import logging
+from pymongo.errors import DuplicateKeyError
 
-from db import get_user
+from db import get_user, save_user
 
 app = Flask(__name__)
 app.secret_key = 'cokgizli'
@@ -38,6 +39,24 @@ def login():
     return render_template('login.html', message=message)
 
 
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    message = ''
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        try:
+            save_user(username, email, password)
+            return redirect(url_for('login'))
+        except DuplicateKeyError:
+            message = "User already exists!"
+    return render_template('signup.html', message=message)
+
+
 @app.route("/logout")
 @login_required
 def logout():
@@ -46,6 +65,7 @@ def logout():
 
 
 @app.route('/chat')
+@login_required
 def chat():
     username = request.args.get('username')
     room = request.args.get('room')
